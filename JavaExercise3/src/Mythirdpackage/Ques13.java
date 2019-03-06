d79ac09fc58d4c3f11145b0068e2c8ade7e284cf
shantanu565 <47346685+shantanu565@users.noreply.github.com>
2019-03-06 13:13:24 +0530

    Ques13

--- JavaExercise3/src/Mythirdpackage/Ques13.java
+++ JavaExercise3/src/Mythirdpackage/Ques13.java
@@ -4,5 +4,46 @@ public class Ques13 {
     // Coordinate 2 threads using wait() and notify().
     public static void main(String[] args) {
 
+        ThreadA a=new ThreadA();
+        ThreadB b = new ThreadB();
+        a.start();
+        b.start();
+
+        synchronized(b){
+            try{
+                System.out.println("Waiting for b to complete...");
+                b.wait();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            System.out.println("Total is: " + b.vqlue);
+        }
     }
+}
+class ThreadA extends Thread{
+    int vqlue;
+    @Override
+    public void run(){
+        synchronized(this){
+            for(int i=0; i<5 ; i++){
+                vqlue += i;
+            }
+            notify();
+        }
+    }
+
+}
+class ThreadB extends Thread{
+    int vqlue;
+    @Override
+    public void run(){
+        synchronized(this){
+            for(int i=0; i<5 ; i++){
+                vqlue += i;
+            }
+            notify();
+        }
+    }
+
 }
